ef6a9859bf3ee22310ec434ba6d3cf9a21665eee
Obed_Banza <obedbanza.m@gmail.com>
2026-02-27 17:15:57 +0100

    Supprime node_modules et autres fichiers ignorés du repo

--- dist/src/api/label/services/label.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * label service
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreService('api::label.label');
